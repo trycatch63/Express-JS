fdd5422ca762e19ec444bf3a1425287bdd0bc331
trycatch63 <pavelcse69@gmail.com>
2023-06-21 12:40:31 +0600

    Added Middleware Concept

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -118,6 +118,44 @@ app.post('/', function (req, res) {
     })
 })
 
+// Middleware Concept
+// Custom middleware function
+const myMiddleware = (req, res, next) => {
+    // Perform some operations
+    console.log('Middleware executed');
+
+    // Call the next middleware or route handler
+    next();
+};
+
+//Application Middleware
+// Register middleware
+// app.use(myMiddleware);
+
+// app.use(function (req, res, next) {
+//     console.log("I am application level middleware");
+//     next();
+// })
+
+//Route Middleware
+app.use('/about', function (req, res, next) {
+    console.log("I am about page middleware");
+
+    next();
+})
+
+app.get("/home", function (req, res) {
+    res.send("This is home page");
+})
+
+app.get("/about", function (req, res) {
+    res.send("This is about page");
+})
+
+app.get("/contact", function (req, res) {
+    res.send("This is contact page");
+})
+
 app.listen(8000, function () {
     console.log("Server Run Success");
 })
